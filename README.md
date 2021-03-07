1378dd8977cd8bea22c9caddb1679d0aca2b4169
turulomio <turulomio@yahoo.es>
2021-03-07 06:52:29 +0100

    ancient-0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ancient
+# Ancient
 Moves ancient files from a directory to another selecting is age. Then you can backup them.
+
+# Links
+
+Project web page
+    https://github.com/turulomio/ancient
+
+Doxygen documentation:
+    http://turulomio.users.sourceforge.net/doxygen/ancient/
+
+Pypi web page:
+    https://pypi.org/project/ancient/
+
+# Changelog
+
+## 0.1.0
+    - First version and works fine
--- ancient/version.py
+++ ancient/version.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from gettext import translation
 from pkg_resources import resource_filename
 
-__versiondatetime__ = datetime(2020, 9, 9)
+__versiondatetime__ = datetime(2021, 3, 7)
 __versiondate__=__versiondatetime__.date()
 __version__ = '0.1.0'
 
@@ -15,4 +15,4 @@ except:
 ## Function used in argparse_epilog
 ## @return String
 def argparse_epilog():
-    return _("Developed by Mariano Muñoz 2020-{}").format(__versiondate__.year)
+    return _("Developed by Mariano Muñoz 2021-{}").format(__versiondate__.year)
